1a0a2e16f6afadc002a60e129124fb120a2f9d46
BA Partners,Inc <www52012@outlook.com>
2025-11-29 00:25:39 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Backend running'));
 app.use('/api', verifyRouter);
 
-// Render 和大多数 host 通过 process.env.PORT 提供端口
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
